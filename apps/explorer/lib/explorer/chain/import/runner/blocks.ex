97881b6b41e55710de792e812dcf26c33e86675c
Viktor Baranov <baranov.viktor.27@gmail.com>
2021-05-03 10:46:37 +0300

    Change order of balances records for updating
    (cherry picked from commit 947e22041f35448ff33620dff93575705289a221)

--- apps/explorer/lib/explorer/chain/import/runner/blocks.ex
+++ apps/explorer/lib/explorer/chain/import/runner/blocks.ex
@@ -392,10 +392,10 @@ defmodule Explorer.Chain.Import.Runner.Blocks do
         select: map(tb, [:address_hash, :token_contract_address_hash, :token_id, :block_number]),
         # Enforce TokenBalance ShareLocks order (see docs: sharelocks.md)
         order_by: [
-          tb.token_type,
           tb.token_contract_address_hash,
           tb.token_id,
           tb.address_hash,
+          tb.token_id,
           tb.block_number
         ],
         lock: "FOR UPDATE"
@@ -469,7 +469,6 @@ defmodule Explorer.Chain.Import.Runner.Blocks do
         select: map(ctb, [:address_hash, :token_contract_address_hash, :token_id]),
         # Enforce CurrentTokenBalance ShareLocks order (see docs: sharelocks.md)
         order_by: [
-          ctb.token_type,
           ctb.token_contract_address_hash,
           ctb.token_id,
           ctb.address_hash
