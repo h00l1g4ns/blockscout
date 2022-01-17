0249a84e697b04e4af50463dbb2c1139dfbb2805
enrique <enrique@keyko.io>
2022-01-17 12:54:22 +0100

    Add to default

--- apps/block_scout_web/lib/block_scout_web/csp_header.ex
+++ apps/block_scout_web/lib/block_scout_web/csp_header.ex
@@ -12,13 +12,13 @@ defmodule BlockScoutWeb.CSPHeader do
     Controller.put_secure_browser_headers(conn, %{
       "content-security-policy" => "\
         connect-src 'self' #{websocket_endpoints(conn)} wss://*.bridge.walletconnect.org/ https://cdn.segment.com https://api.segment.io https://raw.githubusercontent.com/trustwallet/assets/ https://registry.walletconnect.org/data/wallets.json; \
-        default-src 'self';\
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://cdn.segment.com https://api.segment.io \
-        style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com;\
+        default-src 'self' https://cdn.segment.com https://api.segment.io; \
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://cdn.segment.com https://api.segment.io; \
+        style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com; \
         img-src 'self' * data:;\
         media-src 'self' * data:;\
         font-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.gstatic.com data:;\
-        frame-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://datastudio.google.com/ https://makerdojo.io/ ;\
+        frame-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://datastudio.google.com/ https://makerdojo.io/;\
       "
     })
   end
