c5413ccfd4d3f485eac85a6430a1864586dacc77
Donald Hutchison <donald.hutchison@clabs.co>
2022-08-05 10:26:10 +0200

    More refactoring.

--- apps/explorer/lib/explorer/celo/events/event_transformer.ex
+++ apps/explorer/lib/explorer/celo/events/event_transformer.ex
@@ -1,5 +1,5 @@
 defmodule Explorer.Celo.Events.Transformer do
-  @moduledoc "Transform Explorer.Chain.Log + abi to a decoded event instance"
+  @moduledoc "Transform Explorer.Chain.Log + Event ABI to a human readable map of parameters"
 
   alias Explorer.Chain.Log
   alias Explorer.Celo.ContractEvents.Common
@@ -16,32 +16,9 @@ defmodule Explorer.Celo.Events.Transformer do
       [inputs, indexed, types]
       |> Enum.zip()
 
-    # decode indexed parameters directly from topics
-    indexed_params =
-      event_params
-      |> Enum.filter(fn {_name, indexed, _type} -> indexed == true end)
-      |> Enum.zip([second_topic, third_topic, fourth_topic])
-      |> Enum.into(%{}, fn {{name, _indexed, type}, topic} ->
-        {String.to_atom(name), Common.decode_event_topic(topic, type)}
-      end)
-
-    # decode unindexed parameters from the log data body via their declaration order within the event abi
-    unindexed_param_specs =
-      event_params
-      |> Enum.filter(fn {_, indexed, _} -> indexed == false end)
-
-    unindexed_param_values =
-      unindexed_param_specs
-      |> Enum.map(fn {_name, _indexed, type} -> type end)
-      |> then(&Common.decode_event_data(data, &1))
-
-    unindexed_params =
-      [unindexed_param_specs, unindexed_param_values]
-      |> Enum.zip()
-      |> Enum.into(%{}, fn {{name, _i, _type}, value} -> {String.to_atom(name), value} end)
-
-    # reconcile indexed and unindexed
-    unindexed_params |> Map.merge(indexed_params)
+    event_params
+    |> decode_indexed([second_topic, third_topic, fourth_topic])
+    |> Map.merge(decode_unindexed(event_params, data))
   end
 
   # decode json string
@@ -56,6 +33,33 @@ defmodule Explorer.Celo.Events.Transformer do
     event_abi
     |> FunctionSelector.parse_specification_item()
     |> decode(log)
+  end
+
+  # relevant_topics are the second to fourth topics - the first topic is always the event identifier
+  defp decode_indexed(event_params, relevant_topics) do
+      event_params
+      |> Enum.filter(fn {_name, indexed, _type} -> indexed == true end)
+      |> Enum.zip(relevant_topics)
+      |> Enum.into(%{}, fn {{name, _indexed, type}, topic} ->
+        {String.to_atom(name), Common.decode_event_topic(topic, type)}
+      end)
+  end
+
+  defp decode_unindexed(event_params, data) do
+    unindexed_param_specs =
+      event_params
+      |> Enum.filter(fn {_, indexed, _} -> indexed == false end)
+
+    # get unindexed param values in canonical order
+    unindexed_param_values =
+      unindexed_param_specs
+      |> Enum.map(fn {_name, _indexed, type} -> type end)
+      |> then(&Common.decode_event_data(data, &1))
+
+    # reconcile values with parameter name
+    [unindexed_param_specs, unindexed_param_values]
+    |> Enum.zip()
+    |> Enum.into(%{}, fn {{name, _i, _type}, value} -> {String.to_atom(name), value} end)
   end
 
   defp decode16!(nil), do: nil
