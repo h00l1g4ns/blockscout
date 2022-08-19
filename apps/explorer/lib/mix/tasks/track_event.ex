6514e0ab3b068704a009a2f0e4837c43186afafa
Donald Hutchison <donald.hutchison@clabs.co>
2022-08-19 17:06:32 +0200

    Formatting and credo.

--- apps/explorer/lib/mix/tasks/track_event.ex
+++ apps/explorer/lib/mix/tasks/track_event.ex
@@ -5,31 +5,35 @@ defmodule Mix.Tasks.TrackEvent do
   use Mix.Task
   require Logger
 
-  alias Explorer.Repo
-  alias Explorer.Chain.SmartContract
   alias Explorer.Chain.Celo.ContractEventTracking
+  alias Explorer.Chain.SmartContract
+  alias Explorer.Repo
+  alias Explorer.SmartContract.Helper, as: SmartContractHelper
+  alias Mix.Task, as: MixTask
   import Ecto.Query
 
   def run(args) do
     {options, _args, invalid} =
-      OptionParser.parse(args, strict: [contract_address: :string, topics: :string, event_names: :string, all: :boolean] )
+      OptionParser.parse(args, strict: [contract_address: :string, topics: :string, event_names: :string, all: :boolean])
 
     validate_preconditions(invalid)
 
-    #start ecto repo
-    Mix.Task.run "app.start"
+    # start ecto repo
+    MixTask.run("app.start")
 
     with {:ok, contract} <- get_verified_contract(options[:contract_address]),
          {:ok, tracking_changesets} <- get_changesets(contract, options[:topics], options[:event_names], options[:all]) do
-
       tracking_changesets
       |> Enum.each(fn changeset ->
         case Repo.insert(changeset) do
           {:ok, cet = %ContractEventTracking{}} ->
-            Logger.info("Tracking new instances of #{cet.topic} (#{cet.name}) on contract #{contract.address |> to_string()} (#{contract.name})")
+            Logger.info(
+              "Tracking new instances of #{cet.topic} (#{cet.name}) on contract #{contract.address_hash |> to_string()} (#{contract.name})"
+            )
+
           {:error, %Ecto.Changeset{errors: errors, changes: %{name: name, topic: topic}}} ->
-            Logger.error "Errors found with event #{topic} (#{name})"
-            Logger.error inspect(errors)
+            Logger.error("Errors found with event #{topic} (#{name})")
+            Logger.error(inspect(errors))
         end
       end)
     else
@@ -46,33 +50,39 @@ defmodule Mix.Tasks.TrackEvent do
   defp get_changesets(contract, _topics, names, _all) when is_binary(names) do
     names = names |> String.split(",")
 
-    trackings = names
-    |> Enum.map(fn name ->
-      case ContractEventTracking.from_event_name(contract, name) do
-        cet = %Ecto.Changeset{valid?: true} ->
-          cet
-        %Ecto.Changeset{valid?: false, errors: errors} ->
-          raise "Errors found with event name #{name} - #{errors}"
-        nil ->
-          raise "Event name #{name} not found in contract #{contract.name}"
-      end
-    end)
+    trackings =
+      names
+      |> Enum.map(fn name ->
+        case ContractEventTracking.from_event_name(contract, name) do
+          cet = %Ecto.Changeset{valid?: true} ->
+            cet
+
+          %Ecto.Changeset{valid?: false, errors: errors} ->
+            raise "Errors found with event name #{name} - #{errors}"
+
+          nil ->
+            raise "Event name #{name} not found in contract #{contract.name}"
+        end
+      end)
 
     {:ok, trackings}
   end
 
   defp get_changesets(contract, topics, _names, _all) when is_list(topics) do
-    trackings = topics
+    trackings =
+      topics
       |> Enum.map(fn topic ->
-      case ContractEventTracking.from_event_topic(contract, topic) do
-        cet = %Ecto.Changeset{valid?: true} ->
-          cet
-        %Ecto.Changeset{valid?: false, errors: errors} ->
-          raise "Errors found with event name #{topic} - #{errors}"
-        nil ->
-          raise "Event topic #{topic} not found in contract #{contract.name}"
-      end
-    end)
+        case ContractEventTracking.from_event_topic(contract, topic) do
+          cet = %Ecto.Changeset{valid?: true} ->
+            cet
+
+          %Ecto.Changeset{valid?: false, errors: errors} ->
+            raise "Errors found with event name #{topic} - #{errors}"
+
+          nil ->
+            raise "Event topic #{topic} not found in contract #{contract.name}"
+        end
+      end)
 
     {:ok, trackings}
   end
@@ -89,7 +99,7 @@ defmodule Mix.Tasks.TrackEvent do
       _ -> false
     end)
     |> Enum.map(fn event_abi ->
-      Explorer.SmartContract.Helper.event_abi_to_topic_str(event_abi)
+      SmartContractHelper.event_abi_to_topic_str(event_abi)
     end)
   end
 
@@ -98,7 +108,7 @@ defmodule Mix.Tasks.TrackEvent do
       raise "Invalid options types passed: #{invalid}"
     end
 
-    unless System.get_env( "DATABASE_URL" ) do
+    unless System.get_env("DATABASE_URL") do
       raise "No database connection provided - set DATABASE_URL env variable"
     end
   end
@@ -109,14 +119,13 @@ defmodule Mix.Tasks.TrackEvent do
         {:error, "Invalid format for address hash"}
 
       {:ok, address} ->
-        contract = from(
-          sm in SmartContract,
-          where: sm.address_hash == ^address)
-        |> Repo.one()
+        query = from(sm in SmartContract, where: sm.address_hash == ^address)
+        contract = query |> Repo.one()
 
         case contract do
           sm = %SmartContract{} ->
             {:ok, sm}
+
           nil ->
             {:error, "No verified contract found at address #{address_string}"}
         end
--- apps/explorer/priv/repo/migrations/20220819140851_unique_tracking_rows.exs
+++ apps/explorer/priv/repo/migrations/20220819140851_unique_tracking_rows.exs
@@ -2,6 +2,6 @@ defmodule Explorer.Repo.Migrations.UniqueTrackingRows do
   use Ecto.Migration
 
   def change do
-    create unique_index(:clabs_contract_event_trackings, [:smart_contract_id, :topic], name: :smart_contract_id_topic)
+    create(unique_index(:clabs_contract_event_trackings, [:smart_contract_id, :topic], name: :smart_contract_id_topic))
   end
 end
