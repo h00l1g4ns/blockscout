97a1ee7e4b39be3d2947015ca98ee500de5ae461
Donald Hutchison <donald.hutchison@clabs.co>
2022-12-19 16:34:30 +0200

    Cache full tx / no tx variants.

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/cache.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/cache.ex
@@ -6,15 +6,17 @@ defmodule EthereumJSONRPC.Cache do
 
   # don't cache call for "latest" block
   def cached_request(%{method: "eth_getBlockByNumber", params: ["latest", _]}), do: false
-  # cache call for block number with full tx content
-  def cached_request(%{method: "eth_getBlockByNumber", params: [block_number, true]}), do: lookup({"eth_getBlockByNumber", block_number})
+  # cache call for block number
+  def cached_request(%{method: "eth_getBlockByNumber", params: [block_number, include_tx?]}) do
+    lookup({"eth_getBlockByNumber", block_number, include_tx?})
+  end
   # don't cache anything else
   def cached_request(_), do: false
 
   @block_by_number_ttl :timer.minutes(5)
-  def store_response(response, %{method: "eth_getBlockByNumber", params: [block_number, true]}) do
+  def store_response(response, %{method: "eth_getBlockByNumber", params: [block_number, include_tx?]}) do
     Logger.info("Store #{block_number}")
-    put({"eth_getBlockByNumber", response}, ttl: @block_by_number_ttl)
+    put({"eth_getBlockByNumber", response, include_tx?}, ttl: @block_by_number_ttl)
     response
   end
   def store_response(response, _request_params), do: response
@@ -23,8 +25,6 @@ defmodule EthereumJSONRPC.Cache do
     case get(key) do
       nil -> nil
       response ->
-        Logger.info("Cache hit ")
-
         {:ok, response}
     end
   end
