0d60d551abf3dae6793455f47a345795d0d838dc
Javier Cortejoso <javier.cortejoso@gmail.com>
2023-04-19 17:27:26 +0200

    debug

--- bin/install_chrome_headless.sh
+++ bin/install_chrome_headless.sh
@@ -7,5 +7,5 @@ unzip chromedriver_linux64.zip
 sudo chmod +x chromedriver
 sudo mv chromedriver /usr/local/bin
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
 sudo apt-get install libstdc++6
